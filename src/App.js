f7f403bd085a3e3c777514ec784de1ee4ea83fa0
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-04 21:44:37 +0200

    finalizing app.js file

--- src/App.js
+++ src/App.js
@@ -31,9 +31,37 @@ const App = () => {
         <div className={
           `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ?'md:ml':'flex-2'}`
         }>
-          <div className='fixed'>
-
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
           </div>
+        </div>
+        <div>
+          <Routes>
+            {/* Dashboard */}
+            <Route path='/' element='ECommerce'/>
+            <Route path='/ecommerce' element='ECommerce'/>
+            {/* pages */}
+            <Route path='/orders' element = "Orders"/>
+            <Route path='/employees' element = "Employees"/>
+            <Route path='/customers' element = "Customers"/>
+            
+            {/* Apps */}
+            <Route path='/kanban' element = "Kanban"/>
+            <Route path='/editor' element = "Editor"/>
+            <Route path='/calendar' element = "Calendar"/>
+            <Route path='/color-picker' element = "ColorPicker"/>
+
+            {/* Charts */}
+
+            <Route path='/line' element = "Line"/>
+            <Route path='/area' element = "Area"/>
+            <Route path='/bar' element = "Bar"/>
+            <Route path='/pie' element = "Pie"/>
+            <Route path='/financial' element = "Financial"/>
+            <Route path='/color-mapping' element = "ColorMapping"/>
+            <Route path='/pyramid' element = "Pyramid"/>
+            <Route path='/stacked' element = "Stacked"/>
+          </Routes>
         </div>
       </div>
       </BrowserRouter>
