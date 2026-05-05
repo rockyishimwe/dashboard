618901907a5c6b1f5fce96627032bb50d54f5af8
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-05 20:05:56 +0200

    addes statuses

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,16 @@ const Navbar = () => {
    return (
     <div className='flex justify-between p-2 md:ml-6 md:mr-6 relative'>
       <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />}/>
-      <div>
+      <div className='flex'>
+        <NavButton title="Cart" customFunc={()=>handleClick('cart')} color={currentColor} icon={<FiShoppingCart/>}/>
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={()=>handleClick('chat')} color={currentColor} icon={<FiShoppingCart/>}/>
+        <NavButton title="Notification" dotColor="rgb(254,201,15)" customFunc={()=>handleClick('notification')} color={currentColor} icon={<RiNotification3Line/>}/>
+        <TooltipComponent content="Profile" position='BottomCenter'>
+          <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={()=>handleClick('userProfile')}>
+
+          </div>
+
+        </TooltipComponent>
 
       </div>
 
