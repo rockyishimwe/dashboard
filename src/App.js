3cbca99c95ffac2551dec6513fa03a0d21deed3b
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-05 19:42:00 +0200

    implemented a navbar section

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 
 
 const App = () => {
-  const activeMenu = true;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings } = useStateContext();
   return (
     <div>
       <BrowserRouter>
