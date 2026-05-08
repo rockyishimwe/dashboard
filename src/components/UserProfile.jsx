122a457415dfdb60b62b74457e41ce7cc2851338
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-08 21:33:02 +0200

    changed the names

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -18,6 +18,14 @@ const UserProfile = () =>{
           borderRadius="50%"
           />
       </div>
+      <div className='flex gap-5 items-center mt-6 border-color border-b-1 pb-6'>
+        <img className='rounded-full h-24 w-24' src={avatar} alt='user-profile'/>
+      </div>
+      <div>
+        <p className='font-semibold text-xl dark:text-gray-200'>Awk Rocky </p>
+        <p className='text-gray-500 text-sm dark:text-gray-400'> Administrator</p>
+        <p className='text-gray-500 text-sm dark:text-gray-400'>Awk-3@gmail.com</p>
+      </div>
     </div>
   )
 };
