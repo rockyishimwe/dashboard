c7e1623d786927360a4698937ecf3f4fa86cffe2
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-08 16:31:59 +0200

    implemented line page

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { ChartsHeader,LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <h1>Line Chart</h1>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category="Line" title="Inflation Rate"/>
+      <div className='w-full'>
+        <LineChart/>
+
+      </div>
+
     </div>
   );
 };
