0de90d4d85e22afad2a1bfc767634093355b34c0
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-08 14:42:40 +0200

    implemented kanban page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-24">
 <p className='dark:text-gray-200 text-gray-700 text-center m-20'>
-  © 2022 All rights reserved by Shoppy.com
+  © 2026 All rights reserved by Shoppy.com
 </p>
       Footer
     </div>
