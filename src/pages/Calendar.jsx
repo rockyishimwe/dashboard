3edffa2f2a03dc1e432fa0996e0a36881bf2a0b5
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-08 09:09:06 +0200

    implemented the calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
-import {ScheduleComponent,ViewsDirective,ViewDirective,Day,Week,WorkWeek,Month,Agenda,Inject,Resize,DragAndDrop} from '@syncfusion/ej2-react-schedule';
-import {scheduleData} from '../data/dummy';
-import {Header} from '../components';
+import React, { useRef } from 'react';
+import { ScheduleComponent, ViewsDirective, ViewDirective, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
 
-//eslint-disable-next-line react/destructuring-assignment
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
 
-const PropertyPane = (props) => <div className='mt-5'>{props.children}</div>;
+const Scheduler = () => {
+  const scheduleObj = useRef(null);
 
-const Scheduler = () =>{
-  const [scheduleObj,setScheduleObj] = useState();
-  const change = (args) =>{
-    scheduleObj.selectedDate = args.value;
-    scheduleObj.dataBind();
+  const change = (args) => {
+    scheduleObj.current.selectedDate = args.value;
+    scheduleObj.current.dataBind();
   };
-  const onDragStart = (arg)=>{
+
+  const onDragStart = (arg) => {
     arg.navigation.enable = true;
   };
-  return (
-    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
 
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="650px"
+        ref={scheduleObj}
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        dragStart={onDragStart}
+      >
+        <ViewsDirective>
+          {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => (
+            <ViewDirective key={item} option={item} />
+          ))}
+        </ViewsDirective>
+        <Inject services={[Day,Week,WorkWeek,Month,Agenda,Resize,DragAndDrop]} />
+      </ScheduleComponent>
     </div>
   )
 }
