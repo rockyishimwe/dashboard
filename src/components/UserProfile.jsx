25265e6cbdd6c25b2f59a4cafc825050afa6f889
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-09 10:28:41 +0200

    starting implementing new icon and also finalizing the userprofile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -20,11 +20,27 @@ const UserProfile = () =>{
       </div>
       <div className='flex gap-5 items-center mt-6 border-color border-b-1 pb-6'>
         <img className='rounded-full h-24 w-24' src={avatar} alt='user-profile'/>
-      </div>
       <div>
         <p className='font-semibold text-xl dark:text-gray-200'>Awk Rocky </p>
         <p className='text-gray-500 text-sm dark:text-gray-400'> Administrator</p>
         <p className='text-gray-500 text-sm dark:text-gray-400'>Awk-3@gmail.com</p>
+      </div>
+      </div>
+      <div>
+        {userProfileData.map((item,index)=>(
+          <div key={index} className='flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer dark:hover:bg-[#42464D]'>
+            <button type='button' style={{color:item.iconColor,backgroundColor:item.iconBg}} className='text-xl rounded-lg p-3 hover:bg-light-gray'>
+              {item.icon}
+            </button>
+            <div>
+              <p className='font-semibold dark:text-gray-200'>{item.title}</p>
+              <p className='text-gray-500 text-sm dark:text-gray-400'>{item.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className='mt-5'>
+        <Button className='mt-5' color='white' bgColor={currentColor} text='Logout' borderRadius="15px" width="full"/>
       </div>
     </div>
   )
