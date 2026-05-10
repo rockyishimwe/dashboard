bfdce122da8fb9433fb864d58636a09122281924
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-10 09:13:09 +0200

    start implementation of pie component

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { pieChartData } from '../../data/dummy';
+import { ChartsHeader,Pie as PieChart} from '../../components';
 
 const Pie = () => {
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <h1>Pie Chart</h1>
-    </div>
+   <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+    <ChartsHeader category="Pie" title="Project Cost Breakdown"/>
+
+
+   </div>
   );
 };
 
