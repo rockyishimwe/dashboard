b50af27286681c9843bb91c15fc8dc8b2bbc91f0
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-10 08:58:44 +0200

    implemented the pie component

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {AccumulationChartComponent,AccumulationSeriesCollectionDirective,AccumulationSeriesDirective,PieSeries,AccumulationDataLabel,Inject,AccumulationTooltip, AccumulationLegend} from '@syncfusion/ej2-react-charts';
+import {useStateContext} from '../../contexts/ContextProvider';
 
-const Pie = () => {
-  return (
-    <div>Pie Chart</div>
-  );
-};
+const Doughnut = ({id,data,legendVisibility,height}) =>{
+  const {currentMode} = useStateContext();
 
-export default Pie;
+  return(
+    <AccumulationChartComponent id={id} legendSettings={{visible:legendVisibility,background:'white'}} height={height} background={currentMode === 'Dark'? '#33373E':'#fff'} tooltip={{enable:true}}>
+      <Inject services={[AccumulationLegend,PieSeries,AccumulationDataLabel,AccumulationTooltip]}/>
+    </AccumulationChartComponent>
+  )
+}
+export default Doughnut;
