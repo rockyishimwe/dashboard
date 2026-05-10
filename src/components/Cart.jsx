da7de2a6669f1eec6ce7a0fb1d68a7d62a8dbdf4
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-10 10:35:14 +0200

    starting implementing new add cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
-import { AiOutlinePlus } from 'react-icons/ai';
-
+import { AiOutlinePlus,AiOutlineMinus } from 'react-icons/ai';
+import { useStateContext } from '../contexts/ContextProvider';
+import { cartData } from '../data/dummy';
+import {Button} from '.';
 
 const Cart = () => {
+  const {currentColor} = useStateContext();
   return (
-    <div className="cart">Cart</div>
+    <div className='bg-half-transparent w-full fixed nav-item top-0 right-0'>
+      <div className='float-right h-screen duration-1000 ease-in-out dark:text-gray-200 transition-all dark:bg-[#484B52] bg-white md:w-400 p-8'>
+        <p className='font-semibold text-lg'>Shopping Cart</p>
+        <Button icon={<MdOutlineCancel/>} color="rgb(153,171,180)" bgHoverColor="light-gray" size="2xl" borderRadius="50%"/>
+      </div>
+    </div>
   );
 };
 
