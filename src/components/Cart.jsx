755576efc5400986e1e4487dd6fb215dc7ca04fb
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-10 14:10:01 +0200

    starting implementing cart component: prices and total

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -23,13 +23,24 @@ const Cart = () => {
               <div className='flex gap-4 mt-2 items-center'>
                 <p className='font-semibold text-lg'>{item.price}</p>
                 <div className='flex items-center border-1 border-r-0 border-color rounded'>
-
+                  <p className='p-2 border-r-1 dark:border-gray-600 border-color text-red-600'><AiOutlineMinus/></p>
+                  <p className='p-2 border-r-1 border-color dark:border-gray-600 text-green-600'>0</p>
+                  <p className='p-2 border-r-1 border-color dark:border-gray-600 text-green-600'><AiOutlineMinus/></p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       ))}
+      <div className='mt-3 mb-3'>
+        <div className='flex justify-between items-center'>
+          <p className='text-gray-500 dark:text-gray-200'>Sub Total</p>
+          <p className='font-semibold'>$890</p>
+        </div>
+      </div>
+      <div className='mt-5'>
+
+      </div>
     </div>
   );
 };
