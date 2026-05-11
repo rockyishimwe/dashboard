82f1395f536e81425f7d158296d1bdbbb714f4cf
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-11 19:43:51 +0200

    starting theme settings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -8,9 +8,13 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
   const {setColor,SetMode,currentColor,currentMode,setThemeSettings} = useStateContext();
-  return (
-    <div>ThemeSettings</div>
-  );
+  return(
+    <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
+      <div className='float-right h-screen dark:text-gray-200 bg-white dark:bg-[#484B52] w-400'>
+
+      </div>
+    </div>
+  )
 };
 
 export default ThemeSettings;
