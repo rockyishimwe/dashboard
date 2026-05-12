81a59e4899730868dda3955fcb12cda3a8d1f8f5
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-12 21:54:39 +0200

    starting implementation of pyramid component

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import { AccumulationChartComponent,AccumulationSeriesCollectionDirective,AccumulationSeriesDirective,Inject,AccumulationLegend,AccumulationDataLabel,AccumulationTooltip,PyramidSeries,AccumulationSelection} from '@syncfusion/ej2-react-charts';
 import {PyramidData} from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { ChartsHeader } from '../../components';
 const Pyramid = () => {
+  const {currentMode} = useStateContext();
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <h1>Pyramid Chart</h1>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category="Pyramid" title="Food comparison Chart"/>
+      <div className='w-full'>
+        <AccumulationChartComponent id='pyramid-chart' legendSettings={{background:'white'}}>
+
+        </AccumulationChartComponent>
+      </div>
+
     </div>
   );
 };
