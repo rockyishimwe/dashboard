f3751c9b27c66c86fb54de925c2b8439ede1261b
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-12 21:47:57 +0200

    starting implementation of pyramid component

--- src/pages/Charts/Pyramid.jsx
+++ src/pages/Charts/Pyramid.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { AccumulationChartComponent,AccumulationSeriesCollectionDirective,AccumulationSeriesDirective,Inject,AccumulationLegend,AccumulationDataLabel,AccumulationTooltip,PyramidSeries,AccumulationSelection} from '@syncfusion/ej2-react-charts';
+import {PyramidData} from '../../data/dummy';
 const Pyramid = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
