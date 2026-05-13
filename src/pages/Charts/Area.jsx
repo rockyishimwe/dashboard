feaa87c40faa609f5bcc2ae90a2dc89876e83281
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-13 13:06:24 +0200

    Area started being implemented  and going to finalize

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -5,10 +5,15 @@ import { areaCustomSeries,areaPrimaryXAxis,areaPrimaryYAxis } from '../../data/d
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const Area = () => {
-  const {currenMode} = useStateContext();
+  const {currentMode} = useStateContext();
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <h1>Area Chart</h1>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <ChartsHeader category="Area" title="Inflation Rate in Percentage"/>
+      <div className='w-full'>
+        <ChartComponent id='charts' primaryXAxis={areaPrimaryXAxis} primaryYAxis={areaPrimaryYAxis} chartArea={{border:{width:0}}} background={currentMode ===  'Dark' ? '#33373E' :'#fff'} legendSettings={{background:'White'}}>
+          <Inject services={[SplineAreaSeries,DateTime,Legend]}/>
+        </ChartComponent>
+      </div>
     </div>
   );
 };
