2f346d3ec27e8b7250b1be8b084ff8e2bfe83876
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-13 12:59:58 +0200

    Area started being implemented

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,DateTime,SplineAreaSeries,Legend } from '@syncfusion/ej2-react-charts';
+import { ChartsHeader } from '../../components';
+import { areaCustomSeries,areaPrimaryXAxis,areaPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Area = () => {
+  const {currenMode} = useStateContext();
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <h1>Area Chart</h1>
