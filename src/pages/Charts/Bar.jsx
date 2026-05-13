736252083bd296627852a88ac4d499bdd4b95144
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-13 05:46:38 +0200

    Starting the implementation of Bar component

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,Legend,Category,Tooltip,ColumnSeries,DataLabel } from '@syncfusion/ej2-react-charts';
+import { barCustomSeries,barPrimaryXAxis,barPrimaryYAxis } from '../../data/dummy';
+import { ChartsHeader } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const Bar = () => {
+  
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <h1>Bar Chart</h1>
