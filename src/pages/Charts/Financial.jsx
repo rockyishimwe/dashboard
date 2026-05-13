0b9909cb58ae728bbee51ec556bdd375cdd37939
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-13 05:04:57 +0200

    start to implement the financial component

--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import {ChatComponent,SeriesCollectionDirective,SeriesDirective,Inject,DateTime,Logarithmic,Zoom,Crosshair,Tooltip,HiloSeries} from '@syncfusion/ej2-react-charts';
+import { financialChartData,FinancialPrimaryXAxis,FinancialPrimaryYAxis } from '../../data/dummy';
+import {useStateContext} from '../../contexts/ContextProvider';
+import { ChartsHeader } from '../../components';
+
+
+function filterValue(value){
+  if(value.x>=date1){
+    return value.x ,value.high,value.low;
+  }
+}
 const date1 = new Date('2026,06,05');
 const Financial = () => {
   return (
