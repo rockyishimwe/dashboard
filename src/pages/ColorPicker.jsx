e34b550403769311ff0e7a33a3d1d7a163412605
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-14 10:56:39 +0200

    :First implementation of color picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import {Header} from '../components';
 import {ColorPickerComponent} from '@syncfusion/ej2-react-inputs';
 
+const change = (args) =>{
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+};
+const CustomColorPicker = ({id,mode}) => <ColorPickerComponent id={id} mode={mode} modeSwitcher={false}  inline showButtons={false} change={change}/>
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
